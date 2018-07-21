a6202492d015ca901bd21b8f516564007ffeadba
dhyjlas <dhyjlas@163.com>
2018-07-21 10:46:02 +0800

    增加HLS推流方式

--- src/main/java/com/example/livedemo/utils/FFmpegUtils.java
+++ src/main/java/com/example/livedemo/utils/FFmpegUtils.java
@@ -14,6 +14,7 @@ public class FFmpegUtils {
 
     public static void rtmp(LiveStream liveStream, Video video) throws IOException, InterruptedException {
         List<String> command = new ArrayList<>();
+        //rtmp推流
         command.add("D:\\ffmpeg\\bin\\ffmpeg.exe");
         command.add("-re");
         command.add("-i");
@@ -24,6 +25,24 @@ public class FFmpegUtils {
         command.add("flv");
         command.add(liveStream.getVideoStream());
 
+        //hls推流
+//        command.add("D:\\ffmpeg\\bin\\ffmpeg.exe");
+//        command.add("-re");
+//        command.add("-i");
+//        command.add(video.getPath() + video.getName());
+//        command.add("-c:v");
+//        command.add("libx264");
+////        command.add("-s");  //设置分辨率
+////        command.add("720x576");
+//        command.add("-c:a");
+//        command.add("copy");
+//        command.add("-f");
+//        command.add("hls");
+//        command.add("-hls_time");
+//        command.add("10");
+//        command.add("D:\\nginx-rtmp\\temp\\hls\\"+liveStream.getStream()+".m3u8");
+
+        log.info(command.toString());
         Process process = new ProcessBuilder(command).redirectErrorStream(true).start();
         liveStream.setProcess(process);
         new PrintStream(process.getInputStream()).start();
